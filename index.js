e7d30c77f308dc3ae43984b50839b698345901e7
akibbhuiyan <akibbhh@gmail.com>
2023-04-11 08:05:30 +0600

    asyn added c

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-
-app.use("/api/api/dalle", dalleRoutes);
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello From Dall.E" });
-});
+async function run() {
+  try {
+    app.use("/api/api/dalle", dalleRoutes);
+    app.get("/", (req, res) => {
+      res.status(200).json({ message: "Hello From Dall.E" });
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 app.listen(8080, () => console.log("Listening on Port 8080 "));
