512721e29d714c99744f6218fbcd236be038e2de
akibbhuiyan <akibbhh@gmail.com>
2023-06-08 21:12:02 +0600

    ads

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 async function run() {
   try {
-    app.use("/api/api/dalle", dalleRoutes);
+    app.use("/api/v1/dalle", dalleRoutes);
+
     app.get("/", (req, res) => {
       res.status(200).json({ message: "Hello From Dall.E" });
     });
